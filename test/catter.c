82df016a1797eac83f39d7c9765b6c5a1a6f7f4f
David Reiss <dreiss@fb.com>
2015-02-09 10:54:48 -0800

    Convert license to BSD+Patents
    This is the current accepted license for FB Open Source.  This project
    has taken no external contributions.

--- test/catter.c
+++ test/catter.c
@@ -1,4 +1,14 @@
 /*
+ *  Copyright (c) 2009, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+/*
    Demo program for libafdt.
 
    This test program is a simple "remote cat" implementation.  It has two
